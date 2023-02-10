154a08ea955343220c752596a200ef77a008a75a
Haritha Gunarathna <haritha@Harithas-MacBook-Air.local>
2023-02-10 09:41:42 +0530

    Developed backend

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const port = process.env.PORT || 5000;
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false }))
+
+app.use('/openai',require('./routes/openaiRoutes'))
 app.listen(port,()=> console.log(`Server Started on Port ${port}`))
